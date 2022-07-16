341d0e1629efcb8e861f8b97d9aefac8614a560d
kiran-kp <projects@kirankp.com>
2022-07-16 14:48:15 -0700

    use 3d-matrices for matrix type

--- src/raylib.lisp
+++ src/raylib.lisp
@@ -295,28 +295,36 @@
   (m2 :float) (m6 :float) (m10 :float) (m14 :float)
   (m3 :float) (m7 :float) (m11 :float) (m15 :float))
 
-(defmethod translate-into-foreign-memory (object (type matrix-type) pointer)
+(defmethod translate-into-foreign-memory ((object 3d-matrices:mat4) (type matrix-type) pointer)
   (with-foreign-slots ((m0 m1 m2 m3 m4 m5 m6 m7 m8 m9 m10 m11 m12 m13 m14 m15) pointer (:struct %matrix))
-                      (setf m0 (coerce (nth 0 object) 'float))
-                      (setf m1 (coerce (nth 1 object) 'float))
-                      (setf m2 (coerce (nth 2 object) 'float))
-                      (setf m3 (coerce (nth 3 object) 'float))
-                      (setf m4 (coerce (nth 4 object) 'float))
-                      (setf m5 (coerce (nth 5 object) 'float))
-                      (setf m6 (coerce (nth 6 object) 'float))
-                      (setf m7 (coerce (nth 7 object) 'float))
-                      (setf m8 (coerce (nth 8 object) 'float))
-                      (setf m9 (coerce (nth 9 object) 'float))
-                      (setf m10 (coerce (nth 10 object) 'float))
-                      (setf m11 (coerce (nth 11 object) 'float))
-                      (setf m12 (coerce (nth 12 object) 'float))
-                      (setf m13 (coerce (nth 13 object) 'float))
-                      (setf m14 (coerce (nth 14 object) 'float))
-                      (setf m15 (coerce (nth 15 object) 'float))))
+    (setf
+     ;; row 1
+     m0  (3d-matrices:miref4 object 0)
+     m4  (3d-matrices:miref4 object 1)
+     m8  (3d-matrices:miref4 object 2)
+     m12 (3d-matrices:miref4 object 3)
+     ;; row 2
+     m1  (3d-matrices:miref4 object 4)
+     m5  (3d-matrices:miref4 object 5)
+     m9  (3d-matrices:miref4 object 6)
+     m13 (3d-matrices:miref4 object 7)
+     ;; row 3
+     m2  (3d-matrices:miref4 object 8)
+     m6  (3d-matrices:miref4 object 9)
+     m10 (3d-matrices:miref4 object 10)
+     m14 (3d-matrices:miref4 object 11)
+     ;; row 4
+     m3  (3d-matrices:miref4 object 12)
+     m7  (3d-matrices:miref4 object 13)
+     m11 (3d-matrices:miref4 object 14)
+     m15 (3d-matrices:miref4 object 15))))
 
 (defmethod translate-from-foreign (pointer (type matrix-type))
   (with-foreign-slots ((m0 m1 m2 m3 m4 m5 m6 m7 m8 m9 m10 m11 m12 m13 m14 m15) pointer (:struct %matrix))
-                      (list m0 m1 m2 m3 m4 m5 m6 m7 m8 m9 m10 m11 m12 m13 m14 m15)))
+    (3d-matrices:mat m0 m4 m8 m12
+                     m1 m5 m9 m13
+                     m2 m6 m10 m14
+                     m3 m7 m11 m15)))
 
 ;;// Color, 4 components, R8G8B8A8 (32bit)
 ;;typedef struct Color {
